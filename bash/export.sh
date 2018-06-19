a213a1dd5812568f4f453f96d2ad540cc3c10fd7
sesn <sesankar11@gmail.com>
2018-06-19 23:42:53 +0530

    added export functionality

---
+++ bash/export.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+_os="`uname`"
+_now=$(date +"%m_%d_%Y")
+_parent=$PWD
+_file="backup/data_$_now.sql"
+echo $_file
+docker-compose exec db sh -c 'exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"' > $_file
+if [[ $_os == "Darwin"* ]] ; then
+  sed -i '.bak' 1,1d $_file
+else
+  sed -i 1,1d $_file # Removes the password warning from the file
+fi
