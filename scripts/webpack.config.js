0c4188a3b64dcfdfd261d1c8a76424fffadd8f58
sesn <sesankar11@gmail.com>
2018-06-19 23:26:59 +0530

    added webpack and fixed some bugs while running development mode

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -18,69 +18,65 @@ const paths = {
   appSrc: resolveApp('src'),
   appBuild: resolveApp('build'),
   appIndexJs: resolveApp('src/index.js'),
-  appNodeModules: resolveApp('node_modules')
+  appNodeModules: resolveApp('node_modules'),
 };
 
-const DEV = process.env.NODE_ENV === 'developement';
-
-// babel transformation
-const js_loader = {
-  test: /.js?$/,
-  loader: 'babel-loader',
-  include: path.appSrc,
-};
-
-// scss / sass loader
-const sass_loader = {
-  test: /.(scss|css|sass)$/,
-  use: ExtractTextPlugin({
-    fallback: 'style-loader',
-    use: [
-      {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
-          minimize: DEV
-        },
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          ident: 'postcss',
-          plugins: () => [
-            autoprefixer({
-              browsers: [
-                '>1%',
-                'last 4 versions',
-                'Firefox ESR',
-                'not ie < 9',
-              ],
-            }),
-          ],
-        },
-      },
-      'sass-loader',
-    ]
-  })
-}
+const DEV = process.env.NODE_ENV === 'development';
 
 module.exports = {
   bail: !DEV,
-  target: web,
+  // We generate sourcemaps in production. This is slow but gives good results.
+  // You can exclude the *.map files from the build during deployment.
+  target: 'web',
   devtool: DEV ? 'cheap-eval-source-map' : 'source-map',
   entry: [paths.appIndexJs],
   output: {
     path: paths.appBuild,
-    filename: DEV ? 'bundle.js' : 'bundle.[hash:8].js'
+    filename: DEV ? 'bundle.js' : 'bundle.[hash:8].js',
   },
   module: {
     rules: [
-      // Disabling requier.ensure as it's not a standard language feature
+      // Disable require.ensure as it's not a standard language feature.
       { parser: { requireEnsure: false } },
       // Transform ES6 with Babel
-      js_loader,
-      sass_loader,
-    ]
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        include: paths.appSrc,
+      },
+      {
+        test: /.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: !DEV
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+                plugins: () => [
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                  }),
+                ],
+              },
+            },
+            'sass-loader',
+          ],
+        }),
+      },
+    ],
   },
   plugins: [
     !DEV && new CleanWebpackPlugin(['build']),
@@ -108,18 +104,18 @@ module.exports = {
         },
         sourceMap: true,
       }),
-      DEV &&
-        new FriendlyErrorsPlugin({
-          clearConsole: false,
-        }),
-        DEV &&
-          new BrowserSyncPlugin({
-            notify: false,
-            host: 'localhost',
-            port: 4000,
-            logLevel: 'silent',
-            files: ['./*.php'],
-            proxy: 'http://localhost:9009/',
-          }),
+    DEV &&
+      new FriendlyErrorsPlugin({
+        clearConsole: false,
+      }),
+    DEV &&
+      new BrowserSyncPlugin({
+        notify: false,
+        host: 'localhost',
+        port: 4000,
+        logLevel: 'silent',
+        files: ['./*.php'],
+        proxy: 'http://localhost:9009/',
+      }),
   ].filter(Boolean),
 };
